c51e62c4c7035dd762ee4dfbe063a5deb7a9ce79
Diego Rocha Chagas <drochachagas@gmail.com>
2019-05-03 13:01:11 -0300

    Create SpotifyAPI to make requests

--- src/apis/SpotifyAPI.js
+++ src/apis/SpotifyAPI.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
-export default axios.create({
+const SpotifyAPI = axios.create({
   baseURL: 'https://api.spotify.com/v1/search',
   headers: {  
-    "Accept": "application/json", "Content-Type": "application/json", "Authorization": "Bearer BQCdNyn90QraP7__xIZ-nmbzWNuIqCl5n1M9Za9o0xDJ8z-y-msHDlsMAO2sAch41dBMBFXVOtL7Bu-fcvoZOH-fYRv2xK3d9NXluzN3m0j5cBlONgWHuJyt0yDVhy9ZkKCLy_HceNtR0QrerMDOE4xsPslQssXlCpnryg"
+    "Accept": "application/json", 
+    "Content-Type": "application/json", 
+    "Authorization": "Bearer BQCdNyn90QraP7__xIZ-nmbzWNuIqCl5n1M9Za9o0xDJ8z-y-msHDlsMAO2sAch41dBMBFXVOtL7Bu-fcvoZOH-fYRv2xK3d9NXluzN3m0j5cBlONgWHuJyt0yDVhy9ZkKCLy_HceNtR0QrerMDOE4xsPslQssXlCpnryg"
   }
 });
+
+export default SpotifyAPI;
+  
