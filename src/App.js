ab49c6bc898af0c28ff63b8f758bfe8995c98bd3
Diego Rocha Chagas <drochachagas@gmail.com>
2019-05-03 16:27:16 -0300

    Start to fix SpotifyAPI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
 import './App.scss';
+import SpotifyAPI from './apis/SpotifyAPI';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { tracks: []};
+  }
+
+  componentDidMount(){
+    SpotifyAPI.get(`?q=glass&type=track&offset=5`)
+      .then(response => this.setState({tracks: response.data.tracks.items}))
+      .catch(err => console.error(err));
+  }
+
+  render(){
+    console.log(this.state.tracks);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/apis/SpotifyAPI.js
+++ src/apis/SpotifyAPI.js
@@ -1,13 +1,41 @@
 import axios from 'axios';
+const CLIENT_ID = "70358d9ae7f84e6a83754104c6a91319";
+const CLIENT_SECRET = "68217e1cc4bd40b59da415cc977b5edb";
+const REDIRECT_URI = "http://localhost:3000/";
 
-const SpotifyAPI = axios.create({
-  baseURL: 'https://api.spotify.com/v1/search',
-  headers: {  
-    "Accept": "application/json", 
-    "Content-Type": "application/json", 
-    "Authorization": "Bearer BQCdNyn90QraP7__xIZ-nmbzWNuIqCl5n1M9Za9o0xDJ8z-y-msHDlsMAO2sAch41dBMBFXVOtL7Bu-fcvoZOH-fYRv2xK3d9NXluzN3m0j5cBlONgWHuJyt0yDVhy9ZkKCLy_HceNtR0QrerMDOE4xsPslQssXlCpnryg"
-  }
-});
+const SpotifyAPI = async () => {
+  const token = await getToken();
+  return axios.create({
+    baseURL: 'https://api.spotify.com/v1/search',
+    headers: {  
+      "Accept": "application/json", 
+      "Content-Type": "application/json", 
+      "Authorization": `Bearer ${token}`
+    }
+  });
+}
+
+const getToken = () => {
+  return axios({
+    method: 'post',
+    url:'https://accounts.spotify.com/api/token',
+    params: {
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      response_type: "code",
+      grant_type :'authorization_code',
+      redirect_uri: REDIRECT_URI
+    },
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  }).then(token => {
+      return token;
+  }).catch(e=> {
+      return e.response.data;
+  });
+};
+  
 
 export default SpotifyAPI;
   
