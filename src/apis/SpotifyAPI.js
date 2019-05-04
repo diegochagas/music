04301229de1f04dce467844b719b154b05ca70f6
Diego Rocha Chagas <drochachagas@gmail.com>
2019-05-04 17:55:39 -0300

    Remove useless content from SpotifyAPI

--- src/apis/SpotifyAPI.js
+++ src/apis/SpotifyAPI.js
@@ -38,38 +38,5 @@ const getHashParams = hash => {
   });
 }
 
-/*
-axios.create({
-  baseURL: 'https://api.spotify.com/v1/search',
-  headers: {  
-    "Accept": "application/json", 
-    "Content-Type": "application/json", 
-    "token_type": "Bearer",
-    "access_token": getToken()
-  }
-});
-
-const getToken = () => {
-  return axios({
-    method: 'post',
-    url:'https://accounts.spotify.com/api/token',
-    params: {
-      client_id: CLIENT_ID,
-      client_secret: CLIENT_SECRET,
-      response_type: "code",
-      grant_type :'authorization_code',
-      redirect_uri: REDIRECT_URI
-    },
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-  }).then(token => {
-    return token;
-  }).catch(e=> {
-    return e.response.data;
-  });
-};
-*/
-
 export default SpotifyAPI;
   
