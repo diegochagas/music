bd4ae288577c4a028b466207ec55bae3ce1f043c
Diego Rocha Chagas <drochachagas@gmail.com>
2019-05-04 17:09:18 -0300

    Create reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,35 @@
+import { combineReducers } from 'redux';
+import { FETCH_ARTISTS,  FETCH_ALBUMS, FETCH_TRACKS } from '../actions';
+
+export default combineReducers({
+  artists: artistsReducer,
+  albums: albumsReducer,
+  tracks: tracksReducer
+});
+
+const artistsReducer = (state = [], action) => {
+  switch (action.type) {
+    case FETCH_ARTISTS:
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+const albumsReducer = (state = [], action) => {
+  switch (action.type) {
+    case FETCH_ALBUMS:
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+const tracksReducer = (state = [], action) => {
+  switch (action.type) {
+    case FETCH_TRACKS:
+      return action.payload;
+    default:
+      return state;
+  }
+}
