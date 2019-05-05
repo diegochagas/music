04219715697b38760614c40a1ff7373b3775d6fb
Diego Rocha Chagas <drochachagas@gmail.com>
2019-05-05 10:36:21 -0300

    Fix actions payload

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,36 +1,19 @@
 import SpotifyAPI from '../apis/SpotifyAPI';
-
-export const FETCH_ARTISTS = "FETCH_ARTISTS";
-export const FETCH_ALBUMS = "FETCH_ALBUMS";
-export const FETCH_TRACKS = "FETCH_TRACKS";
-
 export const TYPE_ARTISTS = "artist";
 export const TYPE_ALBUMS = "album";
 export const TYPE_TRACKS = "track";
 
 export const fetchArtists = searchTerm => async dispatch => {
-  const response = await SpotifyAPI().get(`?q=${searchTerm}`, {
-    params: {
-      type: TYPE_ARTISTS
-    }
-  });
-  dispatch({type: FETCH_ARTISTS, payload: response.data});
+  const response = await SpotifyAPI().get(`?q=${searchTerm}`, { params: { type: TYPE_ARTISTS } });
+  dispatch({type: TYPE_ARTISTS, payload: response.data.artists.items});
 }
 
 export const fetchAlbums = searchTerm => async dispatch => {
-  const response = await SpotifyAPI().get(`?q=${searchTerm}`, {
-    params: {
-      type: TYPE_ALBUMS
-    }
-  });
-  dispatch({type: FETCH_ALBUMS, payload: response.data.albums.items});
+  const response = await SpotifyAPI().get(`?q=${searchTerm}`, { params: { type: TYPE_ALBUMS } });
+  dispatch({type: TYPE_ALBUMS, payload: response.data.albums.items});
 }
 
 export const fetchTracks = searchTerm => async dispatch => {
-  const response = await SpotifyAPI().get(`?q=${searchTerm}`, {
-    params: {
-      type: TYPE_TRACKS
-    }
-  });
-  dispatch({type: FETCH_TRACKS, payload: response.data.tracks.items});
+  const response = await SpotifyAPI().get(`?q=${searchTerm}`, { params: { type: TYPE_TRACKS } });
+  dispatch({type: TYPE_TRACKS, payload: response.data.tracks.items});
 }
