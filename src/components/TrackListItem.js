8249fa7bf36d95f1a033e36cde181eb57c8626b5
Diego Rocha Chagas <drochachagas@gmail.com>
2019-05-05 17:42:15 -0300

    Insert details on the Album list item component, including album tracks list

--- src/components/TrackListItem.js
+++ src/components/TrackListItem.js
@@ -4,7 +4,6 @@ export default class TrackListItem extends React.Component {
   render() {
     const { name, artists, duration_ms } = this.props.item;
     const { images, albumName } = this.props.item.album;
-    console.log(images);
     return(
       <div className="track-list-item">
         <img src={images.length ? images[0].url : "" } alt="Album folder" />
@@ -15,7 +14,5 @@ export default class TrackListItem extends React.Component {
       </div>
     );
   }
-
   renderArtists = artists => artists.map(artist => <span key={artist.id}>{artist.name}, </span>);
-
 }
