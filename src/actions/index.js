94066917809aef0e6f8eca5df85cc495213eeed1
Diego Rocha Chagas <drochachagas@gmail.com>
2019-05-05 17:37:45 -0300

    Include request to artist latest albums and album tracks

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,18 +2,45 @@ import SpotifyAPI from '../apis/SpotifyAPI';
 export const TYPE_ARTISTS = "artist";
 export const TYPE_ALBUMS = "album";
 export const TYPE_TRACKS = "track";
+export const FETCH_LATEST_ALBUMS = "FETCH_LATEST_ALBUMS";
+export const FETCH_ALBUM_TRACKS = "FETCH_ALBUM_TRACKS";
 
 export const fetchArtists = searchTerm => async dispatch => {
-  const response = await SpotifyAPI().get(`?q=${searchTerm}`, { params: { type: TYPE_ARTISTS } });
+  const response = await SpotifyAPI().get(`/search`, { 
+    params: { 
+      q: searchTerm,
+      type: TYPE_ARTISTS 
+    } 
+  });
   dispatch({type: TYPE_ARTISTS, payload: response.data.artists.items});
 }
 
 export const fetchAlbums = searchTerm => async dispatch => {
-  const response = await SpotifyAPI().get(`?q=${searchTerm}`, { params: { type: TYPE_ALBUMS } });
+  const response = await SpotifyAPI().get(`/search`, { 
+    params: { 
+      q: searchTerm,
+      type: TYPE_ALBUMS 
+    } 
+  });
   dispatch({type: TYPE_ALBUMS, payload: response.data.albums.items});
 }
 
 export const fetchTracks = searchTerm => async dispatch => {
-  const response = await SpotifyAPI().get(`?q=${searchTerm}`, { params: { type: TYPE_TRACKS } });
+  const response = await SpotifyAPI().get(`/search`, { 
+    params: { 
+      q: searchTerm,
+      type: TYPE_TRACKS 
+    } 
+  });
   dispatch({type: TYPE_TRACKS, payload: response.data.tracks.items});
+}
+
+export const fetchLatestAlbums = id => async dispatch => {
+  const response = await SpotifyAPI().get(`/artists/${id}/albums`);
+  dispatch({type: FETCH_LATEST_ALBUMS, payload: response.data.items});
+}
+
+export const fetchAlbumTracks = id => async dispatch => {
+  const response = await SpotifyAPI().get(`/albums/${id}/tracks`);
+  dispatch({type: FETCH_ALBUM_TRACKS, payload: response.data.items});
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import { TYPE_ARTISTS,  TYPE_ALBUMS, TYPE_TRACKS } from '../actions';
+import { TYPE_ARTISTS,  TYPE_ALBUMS, TYPE_TRACKS, FETCH_LATEST_ALBUMS, FETCH_ALBUM_TRACKS } from '../actions';
 
 const artistsReducer = (state = [], action) => {
   switch (action.type) {
@@ -13,9 +13,9 @@ const artistsReducer = (state = [], action) => {
 const albumsReducer = (state = [], action) => {
   switch (action.type) {
     case TYPE_ALBUMS:
-      return action.payload;
-      default:
-      return state;
+    return action.payload;
+    default:
+    return state;
   }
 }
 
@@ -24,12 +24,32 @@ const tracksReducer = (state = [], action) => {
     case TYPE_TRACKS:
     return action.payload;
     default:
-      return state;
-    }
+    return state;
   }
-  
+}
+
+const latestArtistAlbumsReducer = (state = [], action) => { 
+  switch (action.type) {
+    case FETCH_LATEST_ALBUMS:
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+const albumTracksReducer = (state = [], action) => {
+  switch (action.type) {
+    case FETCH_ALBUM_TRACKS: 
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
 export default combineReducers({
   artists: artistsReducer,
   albums: albumsReducer,
-  tracks: tracksReducer
+  tracks: tracksReducer,
+  artistLatestAlbums: latestArtistAlbumsReducer,
+  albumTracks: albumTracksReducer
 });
