ce62fb43b18f03967359ed45d036c3855f2fa00d
Diego Rocha Chagas <drochachagas@gmail.com>
2019-05-05 12:48:18 -0300

    Create track list item component

---
+++ src/components/TrackListItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class TrackListItem extends React.Component {
+  render() {
+    const { name, artists, duration_ms } = this.props.item;
+    const { images, albumName } = this.props.item.album;
+    console.log(images);
+    return(
+      <div className="track-list-item">
+        <img src={images.length ? images[0].url : "" } alt="Album folder" />
+        <span>{name}</span>
+        <div>{this.renderArtists(artists)}</div>
+        <span>{albumName}</span>
+        <span>{duration_ms}</span>
+      </div>
+    );
+  }
+
+  renderArtists = artists => artists.map(artist => <span key={artist.id}>{artist.name}, </span>);
+
+}
