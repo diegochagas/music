4853ae0e23013a4c4ade65506f6b6fb93661341b
Diego Rocha Chagas <drochachagas@gmail.com>
2019-05-05 13:03:58 -0300

    Create render popularity function to artist list item component

--- src/components/ArtistListItem.js
+++ src/components/ArtistListItem.js
@@ -8,11 +8,24 @@ export default class ArtistListItem extends React.Component {
         <img src={images.length ? images[0].url : ""} alt="Song" />
         <span>{name}</span>
         <div>{this.renderGenres(genres)}</div>
-        <span>{popularity}</span>
+        <span>{this.renderPopularity(popularity)}</span>
       </div>
     );
   }
 
   renderGenres = genres => genres.map(genre => <span key={genre}>{genre}, </span>);
   
+  renderPopularity = popularity => {
+    if (popularity > 80) {
+      return "Hot";
+    } else if (popularity >= 60 && popularity <= 79) {
+      return "Cool";
+    } else if (popularity >= 30 && popularity <= 59) {
+      return "Regular";
+    } else if (popularity < 30) {
+      return "Underground";
+    } else {
+      return "";
+    }
+  }
 }
