87e549a9610557ef63263d35074373f0903715a2
Diego Rocha Chagas <drochachagas@gmail.com>
2019-05-07 18:17:03 -0300

    Update FavoriteAPi functions

--- src/apis/FavoriteAPI.js
+++ src/apis/FavoriteAPI.js
@@ -1,43 +1,16 @@
-// let favorites = [];
-
 export const updateFavorite = id => {
+  const isFavorite = getFavorite(id);
+  const newFavoriteStatus = changeFavoriteStatus(id, isFavorite);
+  return {id: id, isFavorite: newFavoriteStatus};
+}
+
+const getFavorite = id => {
   const favorite = localStorage.getItem(id);
-  const isFavorite = favorite === "true" ? true : false;
-  localStorage.setItem(id, (!isFavorite).toString());
-  return {id: id, isFavorite};
-}
-/*
-export const addFavorite = id => {
-  const isFavorite = (newFavorite !== null ) ? newFavorite.isFavorite : false;
-  const stringFavorite = `{"id": "${id}", "isFavorite": ${isFavorite}}`;
-  localStorage.setItem(id, stringFavorite);
-  updateFavoritesList(id);
-  const hasFavorite = favorites.some(favorite => favorite.id === id);
-  if (!hasFavorite) {
-    favorites.push(getFavorite(id));
-  }
+  return favorite === "true" ? true : false;
 }
 
-export const changeFavoriteStatus = id => {
-  const newFavorite = getFavorite(id);
-  const stringFavorite = `{"id": "${id}", "isFavorite": ${!newFavorite.isFavorite}}`;
-  localStorage.setItem(id, stringFavorite);
-  addFavorite(id); 
+const changeFavoriteStatus = (id, isFavorite) => {
+  const favoriteStatus = !isFavorite;
+  localStorage.setItem(id, (favoriteStatus).toString());
+  return favoriteStatus;
 }
-
-const updateFavoritesList = id => {
-  const updatedFavorites = favorites.map(favorite => {
-    const newFavorite = getFavorite(id);
-    if (newFavorite.id === favorite.id) {
-      return newFavorite;
-    }
-    return favorite;
-  });
-  favorites = updatedFavorites;
-}
-
-export const getFavorite = id => JSON.parse();
-
-export const getAllFavorites = () => favorites;
-
-*/
