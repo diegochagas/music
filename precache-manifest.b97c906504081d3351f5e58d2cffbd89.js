7a857e10704b31e0860ebae9cc9de68db8e2905d
Diego Rocha Chagas <drochachagas@gmail.com>
2019-05-07 13:47:06 -0300

    Change icon to favorite when you click

--- precache-manifest.b97c906504081d3351f5e58d2cffbd89.js
+++ precache-manifest.b97c906504081d3351f5e58d2cffbd89.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7fdbcbe898e89e4d4bd9aa5200865a9f",
+    "revision": "82cca6da56acf40c72b2539d25a6b218",
     "url": "/music/index.html"
   },
   {
-    "revision": "d4584a07e4a2fb6a31af",
+    "revision": "ab3a6a90c4ac2ef52ecd",
     "url": "/music/static/css/main.dcf4bee6.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/music/static/js/2.3c353e0d.chunk.js"
   },
   {
-    "revision": "d4584a07e4a2fb6a31af",
-    "url": "/music/static/js/main.7da0e10d.chunk.js"
+    "revision": "ab3a6a90c4ac2ef52ecd",
+    "url": "/music/static/js/main.ffbe1f9b.chunk.js"
   },
   {
     "revision": "3321ad820ee41c1cac90",
