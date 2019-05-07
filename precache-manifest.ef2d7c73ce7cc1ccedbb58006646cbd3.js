2987fc1c32fa65f20afe1962ed3147d044a8ea81
Diego Rocha Chagas <drochachagas@gmail.com>
2019-05-06 22:44:20 -0300

    Deploy

---
+++ precache-manifest.ef2d7c73ce7cc1ccedbb58006646cbd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5adfdf51754e87c25a123c658020e7c7",
+    "url": "/music/index.html"
+  },
+  {
+    "revision": "dba6e8d8591db135d724",
+    "url": "/music/static/css/main.e981010c.chunk.css"
+  },
+  {
+    "revision": "ddef658596f5803291b4",
+    "url": "/music/static/js/2.3c353e0d.chunk.js"
+  },
+  {
+    "revision": "dba6e8d8591db135d724",
+    "url": "/music/static/js/main.71324e61.chunk.js"
+  },
+  {
+    "revision": "3321ad820ee41c1cac90",
+    "url": "/music/static/js/runtime~main.4a84191b.js"
+  },
+  {
+    "revision": "1260c83f081dca1d08decb863e3f8c64",
+    "url": "/music/static/media/noimage.1260c83f.jpg"
+  }
+]);
